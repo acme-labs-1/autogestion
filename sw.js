f137031842c1d64added463fa555d689ef169f27
Acme Labs <bases@estudiointegraldyf.com>
2026-08-18 11:57:03 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+// sw.js - Service Worker
+const CACHE_NAME = 'fyd-panel-v1';
+const urlsToCache = [
+  '/panel.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
